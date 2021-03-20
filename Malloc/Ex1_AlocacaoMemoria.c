13bfc3c4c9c450eed98129e09b8e2a918ac4ca78
FELPs <pablofelipe1999@hotmail.com>
2021-03-20 15:21:33 -0300

    Correcting errors

--- Malloc/Ex1_AlocacaoMemoria.c
+++
@@ -1,8 +0,0 @@
-Um ponteiro e um "apontador", um tipo de especial de variavel que armazena endereços.
-A forma de declaraçao de um ponteiro e:
-int *ponteiro;
-A variavel "ponteiro" e um ponteiro para uma variavel do tipo "int".
-int a=20;
-ponteiro = &a;
-A variavel "ponteiro" armazena o endereço de "a" na memoria, ou aponta (*ponteiro) pa-
-ra o valor da variavel "a".
--- Malloc/Ex2_AlocacaoMemoria.c
+++
@@ -1,2 +0,0 @@
-O operador em linguagem C que nos permite obter o endereço de uma variavel
-e o operador "&".
